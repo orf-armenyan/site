1ea5356d8f9bb50ea6a8b8de0fe5c33e37802f0c
Aram Charkhchyan <aram.charkhchyan@factumsoft.com>
2019-12-29 13:43:13 +0400

    enter

---
+++ blog/database/seeds/ArminOrderProductsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArminOrderProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [];
+		for ($y=1 ; $y <= 12; $y++) {
+			for($i=1; $i<=5;$i++){
+			 $data[] = [
+			 	 'order_id' => $y,
+				 'product_id' => $i,
+				 'qty' => rand(1,4),
+				 'title' => 'Casio test prod',
+				 'price' =>  rand(40,200),
+			 ]
+			}
+		} 
+			DB::table (table 'order_products')->insert(:data);
+    }
+}
